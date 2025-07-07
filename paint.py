24accc1820158b09bf83787d624b0a7449d18f55
zacharesmer <zachresmer@gmail.com>
2025-07-07 15:55:35 -0400

    make history traversal async

--- paint.py
+++ paint.py
@@ -1,9 +1,9 @@
 from pirda.irda import IrDA_UART
 import board_config
 import time
-from screen.basictouch import Touchscreen
+from screen.touch import Touchscreen
 import screen.st7789v as st7789
-from machine import Pin
+from machine import Pin, Timer
 import random
 from array import array
 
@@ -30,7 +30,7 @@ def clear_screen(*args):
     bg_color = random_bg_color()
     # TODO make some fun effects for coloring in the new background color (random lines, dots, etc.)
     tft.fill(bg_color)
-    tft.frame_buf.text(f"{bg_color:016b}", 50, 50, color)
+    # tft.frame_buf.text(f"{bg_color:016b}", 50, 50, color)
     history.clear()
 
 
@@ -45,14 +45,14 @@ def calibrate():
 
 
 class PaintHistory:
+    # TODO: make some kind of frame around the drawing area so it's only 255 x 240 so I can hold more dots in the history
+    # or if I want to be boring just send the framebuf (boooo)
     def __init__(self):
         self.index = 0
         self.latest_valid_index = 0
-        import gc
 
-        print(gc.mem_free())
         self.max_size = 10_000
-        self.sleep_time = 0.004
+        self.sleep_time = 1
 
         self.x = array("i", [0] * 10_000)
         self.y = array("i", [0] * 10_000)
@@ -82,8 +82,29 @@ def go_back(*args):
             time.sleep(history.sleep_time)
 
 
+def go_back_async(*args):
+    # if the button has stopped being pressed, stop going back
+    if args[0].value() != 0:
+        return
+    global history
+    button = args[0]
+    if history.index > 0:
+        history.index -= 1
+        # erase the pixel
+        tft.fill_circle(
+            history.x[history.index],
+            history.y[history.index],
+            3,
+            bg_color,
+        )
+        Timer(
+            mode=Timer.ONE_SHOT,
+            period=history.sleep_time,
+            callback=lambda *args: go_back_async(button),
+        )
+
+
 def go_forward(*args):
-    global tft
     global history
     while args[0].value() == 0:
         if history.index < history.latest_valid_index:
@@ -92,6 +113,22 @@ def go_forward(*args):
             )
             history.index += 1
             time.sleep(history.sleep_time)
+
+
+def go_forward_async(*args):
+    # if the button has stopped being pressed, stop going forward
+    if args[0].value() != 0:
+        return
+    global history
+    button = args[0]
+    if history.index < history.latest_valid_index:
+        tft.fill_circle(history.x[history.index], history.y[history.index], 3, color)
+        history.index += 1
+        Timer(
+            mode=Timer.ONE_SHOT,
+            period=history.sleep_time,
+            callback=lambda *args: go_forward_async(button),
+        )
 
 
 # conversion functions to translate between touch sensor size and screen size
@@ -164,10 +201,10 @@ def set_up_buttons():
     start_button.irq(clear_screen, trigger=Pin.IRQ_FALLING)
 
     a_button = Pin(board_config.A_BUTTON, mode=Pin.IN, pull=Pin.PULL_UP)
-    a_button.irq(go_forward, trigger=Pin.IRQ_FALLING)
+    a_button.irq(go_forward_async, trigger=Pin.IRQ_FALLING)
 
     b_button = Pin(board_config.B_BUTTON, mode=Pin.IN, pull=Pin.PULL_UP)
-    b_button.irq(go_back, trigger=Pin.IRQ_FALLING)
+    b_button.irq(go_back_async, trigger=Pin.IRQ_FALLING)
 
     right_button = Pin(board_config.RIGHT_BUTTON, mode=Pin.IN, pull=Pin.PULL_UP)
     right_button.irq(send_drawing, trigger=Pin.IRQ_FALLING)
@@ -209,7 +246,8 @@ def run():
             history.index = 0
             print("outta space")
         history.latest_valid_index = history.index
-        # time.sleep(0.01)
+        # tft.draw_frame()
+        # time.sleep(1 / 40)
 
 
 run()
