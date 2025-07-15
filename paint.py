3fbc6fc8591f799dd3cfbdb642079dadfcba20c2
zacharesmer <zachresmer@gmail.com>
2025-07-15 15:53:44 -0400

    reserve some space in the message format for future use

--- paint.py
+++ paint.py
@@ -2,7 +2,6 @@
 A program where you can draw and also send your drawing to another person
 """
 
-import board_config
 import time
 from machine import Pin, Timer
 import random
@@ -28,12 +27,15 @@ class Program:
         self.badge = badge
         self.tft = badge.screen
         self.touch = badge.touch
+        self.canvas_x = 319
+        self.canvas_y = 239
         print(f"Available: {gc.mem_free()}")
         # size = min(20_000, round(gc.mem_free() / 50))
         size = 5_000
         print(f"using {size}")
-        self.history = PaintHistory(size)
+        self.history = PaintHistory(size, self.canvas_x, self.canvas_y)
         self.color, self.bg_color = self.random_colors()
+        self.version = 0
 
     def random_colors(self):
         fgh = random.random()
@@ -60,7 +62,7 @@ class Program:
         self.history.clear()
 
     def go_back(self, *args):
-        print(args)
+        # print(args)
         while args[0].value() == 0:
             if self.history.index > 0:
                 # erase the pixel
@@ -117,21 +119,21 @@ class Program:
             )
 
     def send_drawing(self, *args):
+        # message header: version, data size in words, color, 5 words reserved for future use
         self.badge.irda_uart.reset_machines()
         anim = BlinkRed(brightness=100, delay=30)
-        print(f"Sending size: {self.history.index}")
+        self.badge.irda_uart.send_word(self.version)
+        print(f"Sending data size: {self.history.index}")
         send_start = time.time_ns()
         self.badge.irda_uart.send_word(self.history.index)
         asyncio.sleep_ms(self.history.sleep_time)
-        # time.sleep_ms(history.sleep_time)
         self.badge.irda_uart.send_word(self.color.c565)
+        # 5 words of nothing in case theres something to add in a future version
+        for _ in range(5):
+            self.badge.irda_uart.send_word(0)
         asyncio.sleep_ms(self.history.sleep_time)
-        # time.sleep_ms(history.sleep_time)
         for i in range(1, self.history.index + 1):
             self.badge.set_eyes(anim.next())
-            # p_x = history.x[i]
-            # p_y = history.y[i]
-            # print(f"{history.get_point(i):032b}")
             self.badge.irda_uart.send_word(self.history.get_point(i))
 
         print("sent")
@@ -144,30 +146,51 @@ class Program:
         anim = BlinkRed(brightness=100, delay=400)
         self.badge.set_eyes(anim.next())
         self.badge.irda_uart.reset_machines()
+        version = None
         size = None
         other_color = None
         rxed = 0
-        # TODO: actually clear out the fifo, seems like something is wrong idk
-        while (size is None or size == 0xFF_FF_FF_FF) and args[0].value() == 0:
+
+        # message header size: 8 words
+        # so far the format is version, message data size in words, color, 5 words of nothing
+
+        # TODO: actually clear out the fifo, seems like something is wrong and causing me to get 0xFF_FF_FF_FF
+        while (version is None or version == 0xFF_FF_FF_FF) and args[0].value() == 0:
+            version = self.badge.irda_uart.receive_word()
+            self.badge.set_eyes(anim.next())
+        if args[0].value() == 1:
+            print("No version :(")
+            self.badge.set_front(self.color)
+            return
+
+        while (size is None) and args[0].value() == 0:
             size = self.badge.irda_uart.receive_word()
             self.badge.set_eyes(anim.next())
-
-        if size is None or size == 0xFF_FF_FF_FF:
+        if args[0].value() == 1:
             print("No size :(")
             self.badge.set_front(self.color)
             return
-        print(f"{size:032b}")
+
+        print(f"Message data size {size:032b}")
         while other_color is None and args[0].value() == 0:
             other_color = self.badge.irda_uart.receive_word()
-            if other_color is not None:
-                print(other_color)
-            # asyncio.sleep(0)
             self.badge.set_eyes(anim.next())
-
         if other_color is None:
             print("No color :(")
             self.badge.set_front(self.color)
             return
+
+        # ignore the next 5 words because I may want to use them for something in the future
+        for _ in range(5):
+            ignore = None
+            while ignore is None and args[0].value() == 0:
+                ignore = self.badge.irda_uart.receive_word()
+                self.badge.set_eyes(anim.next())
+            # if we stopped holding down the button bail out
+            if args[0].value() == 1:
+                self.badge.set_front(self.color)
+                return
+
         while args[0].value() == 0 and rxed < size:
             self.badge.set_eyes(anim.next())
             val = None
@@ -212,50 +235,40 @@ class Program:
                     prev_x, prev_y = t
                     if self.history.add_x_y(x, y):
                         self.tft.fill_circle(x, y, 3, self.color.c565)
-        self.exit()
-
-    async def exit(self):
-        self.is_running = False
+        # teardown
+        del self.history.points
+        del self.history
         self.badge.start_button.irq(None)
         self.badge.a_button.irq(None)
         self.badge.b_button.irq(None)
         self.badge.right_button.irq(None)
         self.badge.left_button.irq(None)
         self.badge.neopixels.fill((0, 0, 0))
-        del self.history.points
-        del self.history
+        self.badge.irda_uart = None
 
-
-# class Color:
-#     def __init__(self, h, s, v):
-#         # make the LEDs dimmer than the screen and boost their saturation
-#         self.LED = lib.hsv_to_rgb(h, max(s, 0.8), min(v, 0.5))
-#         self.c565 = lib.color565(*lib.hsv_to_rgb(h, s, v))
+    async def exit(self):
+        self.is_running = False
 
 
 class PaintHistory:
     # TODO: make some kind of frame around the drawing area so it's only 255 x 240 so I can hold more dots in the history?
-    def __init__(self, size):
+    def __init__(self, history_size, x, y):
+        self.canvas_x = x
+        self.canvas_y = y
         # index is the most recently written index of the history array
         self.index = 0
-        # this is the latest valid index to fast forward to
+        # this is the latest valid index to fast forward to and write to
         # if we've undone, it will not match the current index
         self.latest_valid_index = 0
 
-        self.max_size = size - 1
+        self.max_size = history_size - 1
         self.sleep_time = 1
 
-        self.points = array("i", [0 for _ in range(size)])
+        self.points = array("i", [0 for _ in range(history_size)])
 
     def clear(self):
         self.index = 0
         self.latest_valid_index = 0
-
-    # def x_at(self, index):
-    #     return self.points[index] >> 16
-
-    # def y_at(self, index):
-    #     return self.points[index] & 0x00_00_FF_FF
 
     def x(self):
         return (self.points[self.index] >> 16) & 0x00_00_FF_FF
@@ -267,17 +280,19 @@ class PaintHistory:
         return self.add_point((x << 16) | (y & 0x00_00_FF_FF))
 
     def add_point(self, p):
-        if self.index >= self.max_size:
-            # history.index = 0
-            print("outta space")
-            return False
-        self.index += 1
-        self.points[self.index] = p
-        self.latest_valid_index = self.index
-        return True
+        # make sure it's actually a valid coordinate before saving it
+        if (
+            p >> 16
+        ) & 0x00_00_FF_FF < self.canvas_x and p & 0x00_00_FF_FF < self.canvas_y:
+            if self.index >= self.max_size:
+                # history.index = 0
+                print("outta space")
+                return False
+            self.index += 1
+            self.points[self.index] = p
+            self.latest_valid_index = self.index
+            return True
+        return False
 
     def get_point(self, index):
         return self.points[index]
-
-
-# PaintProgram().run()
