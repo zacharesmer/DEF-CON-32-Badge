3d00d9b27296f26622855a6b6dca87a32bcc714b
zacharesmer <zachresmer@gmail.com>
2025-07-24 09:20:47 -0400

    add play time elapsed indicator and pause to wav player

--- builtin_programs/wav_player.py
+++ builtin_programs/wav_player.py
@@ -7,7 +7,7 @@ import micropython
 import board_config as bc
 from array import array
 
-micropython.alloc_emergency_exception_buf(100)
+micropython.alloc_emergency_exception_buf(500)
 
 
 class Program(FileBrowserProgram):
@@ -15,7 +15,12 @@ class Program(FileBrowserProgram):
         super().__init__(
             badge, root_dir_name="music", file_extension="wav", create_file=False
         )
-        self.dac = ShittyDAC(self.badge.speaker.pwm)
+        self.dac = ShittyDAC(self.badge)
+        self.run_time_s = 0
+        self.run_time_mins = 0
+        # this is because in an actual hardware interrupt you can't directly schedule a bound method
+        # https://docs.micropython.org/en/latest/reference/isr_rules.html#creation-of-python-objects
+        self.draw_play_pause_ref = self.draw_play_pause
 
     def select(self, arg):
         # print(f"Selecting {self.current_selection}, mode: {self.mode}")
@@ -26,16 +31,21 @@ class Program(FileBrowserProgram):
             super().select(arg)
 
     def setup_playback_buttons(self):
-        self.badge.a_button.irq(self.stop_playing, Pin.IRQ_FALLING, hard=True)
+        # need to set these differently for playback because it blocks the CPU so they
+        # must be actual hardware interrupts
+        super().un_setup_buttons()
+        self.badge.a_button.irq(self.play_pause, Pin.IRQ_FALLING, hard=True)
         self.badge.b_button.irq(self.stop_playing, Pin.IRQ_FALLING, hard=True)
         self.badge.up_button.irq(self.vol_up, Pin.IRQ_FALLING, hard=True)
         self.badge.down_button.irq(self.vol_down, Pin.IRQ_FALLING, hard=True)
 
     def un_setup_playback_buttons(self):
-        self.badge.a_button.irq(None)
-        self.badge.b_button.irq(None)
-        self.badge.up_button.irq(None)
-        self.badge.down_button.irq(None)
+        # the same buttons are all used in the parent class
+        super().setup_buttons()
+
+    def play_pause(self, arg):
+        self.dac.play_pause()
+        micropython.schedule(self.draw_play_pause_ref, True)
 
     def stop_playing(self, arg):
         self.dac.stop_playing()
@@ -52,7 +62,12 @@ class Program(FileBrowserProgram):
         # print(f"Showing, mode is {self.mode}")
         if self.mode == "File":
             try:
-                self.dac.get_wav_metadata(self.open_filename)
+                _, _, frame_rate, nframes = self.dac.get_wav_metadata(
+                    self.open_filename
+                )
+                self.run_time_s = nframes // frame_rate
+                self.run_time_mins = self.run_time_s // 60
+                self.run_time_s %= 60
                 self.badge.screen.frame_buf.fill(self.badge.theme.bg2)
                 self.badge.screen.text_in_box(
                     f"Now playing: {self.open_filename}",
@@ -64,19 +79,18 @@ class Program(FileBrowserProgram):
                     box_width=bc.SCREEN_WIDTH - 10,
                     box_height=40,
                 )
-                # self.badge.screen.frame_buf.text("Play?", 10, 10, self.badge.theme.fg1)
-                triangle_width = 20
-                triangle_height = 20
-                triangle_points = array(
-                    "h",
-                    (0, 0, 0, triangle_height, triangle_width, triangle_height // 2),
+                self.badge.screen.frame_buf.text(
+                    f"00:00",
+                    20,
+                    bc.SCREEN_HEIGHT - 40,
+                    self.badge.theme.fg1,
                 )
-                self.badge.screen.frame_buf.poly(
-                    bc.SCREEN_WIDTH // 2 - triangle_width // 2,
-                    130,
-                    triangle_points,
-                    self.badge.theme.fg3,
-                    True,
+                self.draw_play_pause()
+                self.badge.screen.frame_buf.text(
+                    f"{self.run_time_mins:02}:{self.run_time_s:02}",
+                    bc.SCREEN_WIDTH - 60,
+                    bc.SCREEN_HEIGHT - 40,
+                    self.badge.theme.fg1,
                 )
                 self.badge.screen.draw_frame()
             except ValueError as ve:
@@ -93,12 +107,44 @@ class Program(FileBrowserProgram):
                 box_width=bc.SCREEN_WIDTH - 10,
                 box_height=40,
             )
-            self.badge.screen.frame_buf.rect(
-                bc.SCREEN_WIDTH // 2 - 10, 130, 20, 20, self.badge.theme.fg3, True
+            self.badge.screen.frame_buf.text(
+                f"{self.run_time_mins:02}:{self.run_time_s:02}",
+                bc.SCREEN_WIDTH - 60,
+                bc.SCREEN_HEIGHT - 40,
+                self.badge.theme.fg1,
             )
+            self.draw_play_pause()
             self.badge.screen.draw_frame()
         else:
             super().show(refresh=refresh)
+
+    def draw_play_pause(self, draw_screen=False):
+        self.badge.screen.frame_buf.rect(
+            bc.SCREEN_WIDTH // 2 - 10, 130, 20, 20, self.badge.theme.bg2, True
+        )
+        if self.dac.paused:
+            self.badge.screen.frame_buf.rect(
+                bc.SCREEN_WIDTH // 2 - 10, 130, 5, 20, self.badge.theme.fg3, True
+            )
+            self.badge.screen.frame_buf.rect(
+                bc.SCREEN_WIDTH // 2 + 5, 130, 5, 20, self.badge.theme.fg3, True
+            )
+        else:
+            triangle_width = 20
+            triangle_height = 20
+            triangle_points = array(
+                "h",
+                (0, 0, 0, triangle_height, triangle_width, triangle_height // 2),
+            )
+            self.badge.screen.frame_buf.poly(
+                bc.SCREEN_WIDTH // 2 - triangle_width // 2,
+                130,
+                triangle_points,
+                self.badge.theme.fg3,
+                True,
+            )
+        if draw_screen:
+            self.badge.screen.draw_frame()
 
     async def error(self, message):
         self.un_setup_playback_buttons()
@@ -114,6 +160,6 @@ class Program(FileBrowserProgram):
         self.setup_playback_buttons()
         await self.dac.play_wav(self.open_filename)
         self.mode = "Directory"
-        super().setup_buttons()
+        self.un_setup_playback_buttons()
         # print(f"Done! {self.mode}")
         self.show()
--- lib/dac.py
+++ lib/dac.py
@@ -2,6 +2,7 @@ import time
 import _thread
 from lib.uwave import Wave_read
 import gc
+import board_config as bc
 
 _MIDPOINT = 65536 // 2 - 1
 
@@ -12,16 +13,16 @@ class ShittyDAC:
         ## but it works surprisingly well so hey whatever
         ## framerate: (playback_wait_us, playback_skip)
         8000: (110, 0),
-        11_025: (80, 0),
+        11_025: (70, 0),
         16_000: (50, 0),
         22_050: (28, 0),
         # technically this sort of works but it sounds bad and takes up lots of space for no benefit
-        44_100: (5, 0),
+        # 44_100: (5, 0),
     }
 
-    def __init__(self, pwm, bufsize=4096, pwm_freq=300_000):
-        self.pwm = pwm
-        self.pwm.freq(pwm_freq)
+    def __init__(self, badge, bufsize=4096, pwm_freq=300_000):
+        self.badge = badge
+        self.badge.speaker.pwm.freq(pwm_freq)
         self.bufsize = bufsize
         gc.collect()
         self.bufs = (bytearray(bufsize), bytearray(bufsize))
@@ -35,28 +36,70 @@ class ShittyDAC:
         self.playback_skip = 0
         self.fill_buf_exited = False
         self.volume = 8
+        self.total_buf_fills = None
+        self.buf_fills = 0
+        self.playhead_x = 0
+        self.playhead_y = bc.SCREEN_HEIGHT - 5
+        self.run_time_s = 0
+        self.time_elapsed_s = 0
+        self.time_elapsed_mins = 0
+        self.paused = False
 
     def stop_playing(self):
         self.cancel = True
 
+    def play_pause(self):
+        self.paused = not self.paused
+
     def fill_buf(self):
         while (not self.file_done) and (not self.cancel):
-            if self.ready_to_fill:
+            if self.ready_to_fill and not self.paused:
                 next_playing_buf = (self.playing_buf + 1) % 2
                 # print(f"filling {next_playing_buf}")
                 self.ready_to_fill = False
                 if self.audio_file.readinto(self.bufs[next_playing_buf]) <= 0:
                     self.file_done = True
+                self.buf_fills += 1
+                self.draw_progress()
                 self.ready_to_play = True
         self.fill_buf_exited = True
         print("Done filling!")
+
+    # it feels a little cursed to be doing this in the DAC class, but timing is tight enough that
+    # it's got to happen wherever I can fit it in. I guess I could pass in a method from the GUI class that the DAC
+    # calls and that would feel a little less cursed, but it would be doing the same thing
+    def draw_progress(self):
+        self.playhead_x = round(
+            bc.SCREEN_WIDTH * (self.buf_fills / self.total_buf_fills)
+        )
+        self.badge.screen.fill_circle(
+            self.playhead_x - 1, self.playhead_y, 4, self.badge.theme.fg2
+        )
+        self.badge.screen.fill_circle(
+            self.playhead_x, self.playhead_y, 4, self.badge.theme.accent
+        )
+        self.time_elapsed_s = round(
+            self.run_time_s * (self.buf_fills / self.total_buf_fills)
+        )
+        self.time_elapsed_mins = self.time_elapsed_s // 60
+        self.time_elapsed_s %= 60
+        self.badge.screen.frame_buf.rect(
+            20, bc.SCREEN_HEIGHT - 40, 40, 8, self.badge.theme.bg2, True
+        )
+        self.badge.screen.frame_buf.text(
+            f"{self.time_elapsed_mins:02}:{self.time_elapsed_s:02}",
+            20,
+            bc.SCREEN_HEIGHT - 40,
+            self.badge.theme.fg1,
+        )
+        self.badge.screen.draw_frame()
 
     def play_buf_8u(self):
         # print("playing 8u")
         p = self.playing_buf
         for i in range(0, self.bufsize, self.playback_skip + 1):
             # for sample in self.bufs[p]:
-            self.pwm.duty_u16(self.bufs[p][i] << self.volume)
+            self.badge.speaker.pwm.duty_u16(self.bufs[p][i] << self.volume)
             # print(f"waiting {self.playback_wait_us} us")
             time.sleep_us(self.playback_wait_us)
             # self.last_duty = self.bufs[p][i] << 8
@@ -67,10 +110,12 @@ class ShittyDAC:
         """
         # get_wav_metadata can throw an error, but that is for the GUI to display a message and prevent actually calling this function
         # so the error shouldn't happen this time. Shouldn't...
-        _, _, frame_rate = self.get_wav_metadata(filename)
+        _, _, frame_rate, nframes = self.get_wav_metadata(filename)
         self.playback_wait_us, self.playback_skip = self.u8_frame_rate_adjustments[
             frame_rate
         ]
+        self.run_time_s = nframes // frame_rate
+        self.total_buf_fills = nframes // self.bufsize
         self.audio_file.seek(0x2C, 0)
         _thread.start_new_thread(self.fill_buf, ())
         # poor man's condition variables
@@ -84,9 +129,8 @@ class ShittyDAC:
             self.playing_buf = (self.playing_buf + 1) % 2
             # TODO if this is too slow, inline play_buf?
             # while this thread is playing audio, the second thread will fill up the other buffer and set playing_buf to point to it
-            ## Note: this is just for debugging. The buffer needs to fill up faster than the audio plays or else it can't play in real time
-            while not self.ready_to_play:
-                print("uh oh couldn't load the buffer fast enough")
+            while (not self.ready_to_play or self.paused) and not self.cancel:
+                pass
             self.ready_to_play = False
         print("Done playing!")
         while not self.fill_buf_exited:
@@ -106,6 +150,14 @@ class ShittyDAC:
         self.playback_skip = 0
         # self.last_duty = 0
         self.fill_buf_exited = False
+        self.total_buf_fills = None
+        self.buf_fills = 0
+        self.playhead_x = 0
+        self.playhead_y = bc.SCREEN_HEIGHT - 5
+        self.run_time_s = 0
+        self.time_elapsed_s = 0
+        self.time_elapsed_mins = 0
+        self.paused = False
         for i in range(self.bufsize):
             self.bufs[0][i] = 0
             self.bufs[1][i] = 0
@@ -115,7 +167,7 @@ class ShittyDAC:
         # throw errors if they can't be played
         self.audio_file = open(filename, "rb")
         w = Wave_read(self.audio_file)
-        nchannels, sample_width, frame_rate, *_ = w.getparams()
+        nchannels, sample_width, frame_rate, nframes, *_ = w.getparams()
         w.close()
         if sample_width != 1:
             self.reset()
@@ -130,35 +182,4 @@ class ShittyDAC:
             raise ValueError(
                 f"Invalid frame rate/sample rate: {frame_rate}). Available framerates: {[r for r in self.u8_frame_rate_adjustments.keys()]}"
             )
-        return (nchannels, sample_width, frame_rate)
-
-    # def setup_wav_stuff(self, filename):
-    #     # get metadata for the wave file
-    #     self.audio_file = open(filename, "rb")
-    #     w = Wave_read(self.audio_file)
-    #     nchannels, sample_width, frame_rate, *_ = w.getparams()
-    #     w.close()
-    #     if sample_width != 1:
-    #         self.reset()
-    #         raise ValueError(
-    #             f"Invalid sample width/bit depth: {sample_width}. Valid sample widths: 1 byte (8 bit unsigned)"
-    #         )
-    #     if nchannels != 1:
-    #         self.reset()
-    #         raise ValueError(f"Can't play stereo files (channels: {nchannels})")
-    #     # scale the waiting time with frame rate. Higher framerate = wait less time
-    #     # TODO: at lower frame rates there's a ton of time for something like neopixel animations...
-    #     # Past some frame rate, set an amount of samples to skip because there will be a point where I can't handle them
-    #     print(f"Getting adjustments for frame rate {frame_rate}")
-    #     if sample_width == 1:
-    #         adjustments = self.u8_frame_rate_adjustments.get(frame_rate)
-    #     print(f"Adjsutments: {adjustments}")
-    #     if adjustments is None:
-    #         self.reset()
-    #         raise ValueError(
-    #             f"Invalid frame rate/sample rate: {frame_rate}). Available framerates: {[r for r in self.u8_frame_rate_adjustments.keys()]}"
-    #         )
-    #     else:
-    #         self.playback_wait_us, self.playback_skip = adjustments
-    #     print(f"Wave file metadata: {w.getparams()}")
-    #     # wav data always starts at 0x2c, put the file pointer there
+        return (nchannels, sample_width, frame_rate, nframes)
